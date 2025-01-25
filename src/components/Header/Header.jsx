0f067532de91aa62528fc73cf105900e659e8d0b
farshid pashazadeh <farshidpashazadeh71@gmail.com>
2025-01-25 13:39:29 +0330

    feat(Header Component): Add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">LOGO</div>
+      <div className="header__head-line">descriptive headline</div>
+    </div>
+  );
+}
+
+export default Header;
